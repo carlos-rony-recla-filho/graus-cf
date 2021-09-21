e7ee3b32de47a04ddafdf70fcfdc608ab93d8b06
Carlos Rony <carlosrecla03@gmail.com>
2021-09-20 21:18:59 -0300

    final da tarefa

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace graus_cf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+            Double celsius, fahr;
+
+            Console.WriteLine("bem vindo ao convrsor de graus C° em F°!");
+
+            Console.WriteLine("\nDigite o valor em graus Celsius: ");
+            celsius = Convert.ToDouble(Console.ReadLine());
+
+            fahr = celsius * 1.8 + 32;
+
+            Console.WriteLine($"{celsius}° equivale a {fahr}°F");
+
+        }
+    }
+}
